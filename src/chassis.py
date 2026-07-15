6ae1b686402d9876a288c49c19bbdcaaca03da74
Teerawat Saenum <saenumteerawat@gail.com>
2026-07-15 11:02:45 +0700

    chassis move

--- src/chassis.py
+++ src/chassis.py
@@ -1,1 +1,25 @@
 
+class ChassisController:
+
+    def __init__(self, ep_robot):
+        self.chassis = ep_robot.chassis
+
+    def move_forward(self, distance, speed):
+
+        print(f"[Chassis] Moving forward: distance={distance}m, speed={speed}m/s")
+        self.chassis.move(x=distance, y=0, z=0, xy_speed=speed).wait_for_completed()
+    
+    def move_backward(self, distance, speed):
+
+        print(f"[Chassis] Moving backward: distance={distance}m, speed={speed}m/s")
+        self.chassis.move(x=-distance, y=0, z=0, xy_speed=speed).wait_for_completed()
+
+    def move_turnRight(self, angle, speed):
+
+        print(f"[Chassis] Turning right: angle={angle}, speed={speed}rad/s")
+        self.chassis.move(x=0, y=0, z=-angle, z_speed=45).wait_for_completed()
+
+    def move_turnLeft(self, angle, speed):
+
+        print(f"[Chassis] Turning left: angle={angle}, speed={speed}rad/s")
+        self.chassis.move(x=0, y=0, z=angle, z_speed=45).wait_for_completed()
