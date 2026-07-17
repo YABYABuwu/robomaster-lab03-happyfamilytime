697f58a213d1a32038f48dc7de65d1ea530c26f3
Teerawat Saenum <saenumteerawat@gail.com>
2026-07-17 17:18:07 +0700

    Refactor chassis movement methods in ChassisController

--- src/chassis.py
+++ src/chassis.py
@@ -1,4 +1,3 @@
-
 class ChassisController:
 
     def __init__(self, ep_robot):
@@ -8,18 +7,18 @@ class ChassisController:
 
         print(f"[Chassis] Moving forward: distance={distance}m, speed={speed}m/s")
         self.chassis.move(x=distance, y=0, z=0, xy_speed=speed).wait_for_completed()
-    
+
     def move_backward(self, distance, speed):
 
         print(f"[Chassis] Moving backward: distance={distance}m, speed={speed}m/s")
         self.chassis.move(x=-distance, y=0, z=0, xy_speed=speed).wait_for_completed()
 
-    def move_turnRight(self, angle, speed):
+    def turn_Right(self, angle, speed):
 
         print(f"[Chassis] Turning right: angle={angle}, speed={speed}rad/s")
         self.chassis.move(x=0, y=0, z=-angle, z_speed=45).wait_for_completed()
 
-    def move_turnLeft(self, angle, speed):
+    def turn_Left(self, angle, speed):
 
         print(f"[Chassis] Turning left: angle={angle}, speed={speed}rad/s")
         self.chassis.move(x=0, y=0, z=angle, z_speed=45).wait_for_completed()
